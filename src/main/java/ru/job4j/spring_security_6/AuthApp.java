96e66d6201e5b62fd41ca1fe67d6443433dfe3e0
IvanPavlovets <ivan150287@gmail.com>
2025-04-19 00:13:18 +0700

    init commit

---
+++ src/main/java/ru/job4j/spring_security_6/AuthApp.java
@@ -0,0 +1,13 @@
+package ru.job4j.spring_security_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApp.class, args);
+	}
+
+}
