61e0475c31cc84837e226f9f946312df12bc52bb
IvanPavlovets <ivan150287@gmail.com>
2025-04-20 22:55:40 +0700

    filled the controller

--- src/main/java/ru/job4j/security/controllers/UserController.java
+++ src/main/java/ru/job4j/security/controllers/UserController.java
@@ -1,6 +1,8 @@
 package ru.job4j.security.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.job4j.security.service.UserService;
@@ -10,5 +12,29 @@ import ru.job4j.security.service.UserService;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService service;
+    //private final UserService service;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "This is unprotected page";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String pageForUser() {
+        return "This page only for users";
+    }
+
+    @GetMapping("/admins")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String pageForAdmins() {
+        return "This page only for admins";
+    }
+
+    @GetMapping("/all")
+    public String pageForAll() {
+        return "This page for all employees";
+    }
+
+
 }
